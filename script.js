cc9930ffe957e6b6480abbacd590de175bb85d75
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-09-29 00:18:35 +0600

    add all JavaScript for the app

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 const imgContainer = document.querySelector("#imageGrid");
+const authorSelector = document.getElementById("authorSelector");
 const urlAPI = "https://picsum.photos/v2/list";
 
-const getPicture = () => {
-	fetch(urlAPI)
-		.then((data) => data.json())
-		.then((items) => {
-			console.log(items);
-			items.forEach(function (photoObject) {
-				const newContainer = document.createElement("div");
-				newContainer.classList.add("image_container_item");
+async function fetchImage(url) {
+	const response = await fetch(url);
+	const data = await response.json();
 
-				const newImg = document.createElement("img");
-				newImg.src = photoObject.download_url;
-				newContainer.appendChild(newImg);
+	return data;
+}
 
-				const newAuthor = document.createElement("p");
-				newAuthor.textContent = photoObject.author;
+function populateImages(imageData, imgContainer) {
+	imageData.forEach((item, index, array) => {
+		const imgTag = document.createElement("img");
+		imgTag.src = item.download_url;
 
-				newContainer.appendChild(newAuthor);
-				imgContainer.appendChild(newContainer);
+		imgContainer.appendChild(imgTag);
+	});
+}
+
+function populateDropdown(imageData, authorSelector) {
+	const uniqueAuthor = [];
+
+	imageData.forEach((item) => {
+		const author = item.author;
+
+		if (!uniqueAuthor.includes(author)) {
+			uniqueAuthor.push(author);
+		}
+	});
+
+	const defaultOption = document.createElement("option");
+	defaultOption.value = "";
+	defaultOption.innerText = "Select an option!";
+	authorSelector.appendChild(defaultOption);
+
+	uniqueAuthor.forEach((item) => {
+		const option = document.createElement("option");
+		option.value = item;
+		option.innerText = item;
+
+		authorSelector.appendChild(option);
+	});
+
+	authorSelector.addEventListener("change", (event) => {
+		const crAuthor = event.target.value;
+		// Rest image container first before populating filtered images
+		imgContainer.innerHTML = "";
+
+		if (crAuthor) {
+			const filteredImages = imageData.filter((item) => {
+				return item.author === crAuthor;
 			});
-		});
-};
-getPicture();
+
+			populateImages(filteredImages, imgContainer);
+		} else {
+			populateImages(imageData, imgContainer);
+		}
+	});
+}
+
+// on page load fetch and populate the image data to DOM
+document.addEventListener("DOMContentLoaded", async function () {
+	const imageData = await fetchImage(urlAPI);
+
+	populateImages(imageData, imgContainer);
+
+	populateDropdown(imageData, authorSelector);
+});
